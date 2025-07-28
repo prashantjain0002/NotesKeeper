19b5962be5eec32a16d3ef144bd84bc5a65ba164
Prashant Jain <prashantjain0002@gmail.com>
2025-07-28 17:17:32 +0530

    🐛 Fix error in server.js

--- server/server.js
+++ server/server.js
@@ -23,5 +23,5 @@ app.use("/api/auth", authRoutes);
 app.use("/api/notes", noteRoutes);
 
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
