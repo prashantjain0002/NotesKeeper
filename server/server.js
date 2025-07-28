34562b412fc8e19c9b3296ce1609649c71443ed2
Prashant Jain <prashantjain0002@gmail.com>
2025-07-28 17:02:46 +0530

    🌐 Add CORS policy for deployment

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://notes-keeper-lime.vercel.app",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
